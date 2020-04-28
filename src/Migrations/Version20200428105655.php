f77714f0e93e665103827295570da174d2449c9d
Artem Oliynyk <azure.email@gmail.com>
2020-04-28 14:04:23 +0300

    migration rename fix

--- src/Migrations/Version20200428105655.php
+++ src/Migrations/Version20200428105655.php
@@ -22,7 +22,7 @@ final class Version20200428105655 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE country_cases RENAME TO country_cases_change;');
+        $this->addSql('ALTER TABLE cases_change RENAME TO country_cases_change;');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20200428105655 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE country_cases_change RENAME TO country_cases;');
+        $this->addSql('ALTER TABLE country_cases_change RENAME TO cases_change;');
     }
 }
